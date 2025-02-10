2aeb606afdce4672597d5865eb0e2e0bbc6813c2
hoang10072 <toviethoang.ntt.2020@gmail.com>
2025-02-11 00:59:46 +0700

    Add AdminDAO and Admin model with CRUD methods for admin management

--- src/java/dal/AdminDAO.java
+++ src/java/dal/AdminDAO.java
@@ -4,10 +4,66 @@
  */
 package dal;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Admin;
+
 /**
  *
  * @author tovie
  */
-public class AdminDAO {
+public class AdminDAO extends DBContext {
+
+    public List<Admin> getAllAdmins() {
+        try {
+            List<Admin> admins = new ArrayList<>();
+            String sql = "select * from admin";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Admin admin = new Admin(
+                        rs.getInt("AdminID"),
+                        rs.getString("Name"),
+                        rs.getString("Email"),
+                        rs.getString("Password")
+                );
+                admins.add(admin);
+            }
+            return admins;
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     
 }
+  public Admin getAdminByID(int id) {
+        try {
+            Admin admin = null;
+            String sql = "select * from admin where AdminID=?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                admin = new Admin(
+                        rs.getInt("AdminID"),
+                        rs.getString("Name"),
+                        rs.getString("Email"),
+                        rs.getString("Password")
+                );
+            }
+            return admin;
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+  
+  }
+    public static void main(String[] args) {
+        AdminDAO dao = new AdminDAO();
+        System.out.println(dao.getAdminByID(1));
+    }
+}
+  
