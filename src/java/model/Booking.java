374a86a22fa5cc15f8891c8bdf3a218325522be8
Hieutt0104 <hieutthe173338@fpt.edu.vn>
2025-02-18 02:55:22 +0700

    them model

---
+++ src/java/model/Booking.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.Date;
+
+
+public class Booking {
+    private int bookingID;
+    private int customerID;
+    private Date bookingDate;
+    private double totalPrice;
+
+    public Booking() {
+    }
+
+    public int getBookingID() {
+        return bookingID;
+    }
+
+    public void setBookingID(int bookingID) {
+        this.bookingID = bookingID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public Date getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    
+    
+}
